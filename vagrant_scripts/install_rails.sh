11d3349cf3af14382cfb9bcabfa13ba4380da5ca
batako <guri725ren213@gmail.com>
2018-02-18 11:02:03 +0900

    Add setup SECRET_KEY_BASE to rails provisioner

--- vagrant_scripts/install_rails.sh
+++ vagrant_scripts/install_rails.sh
@@ -7,3 +7,8 @@ echo Installing rails...
 ~/.rbenv/shims/gem install bundler
 cd ~/blockly
 ~/.rbenv/shims/bundle install
+
+echo Setting Up SECRET_KEY_BASE...
+if [ -z $(grep 'SECRET_KEY_BASE' ~/.bash_profile) ] ; then
+  echo 'export SECRET_KEY_BASE=$(cd ~/blockly; bundle exec rake secret)' >> ~/.bash_profile
+fi

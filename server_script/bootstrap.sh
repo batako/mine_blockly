e7806fdfd2b31fe7843e64abcb939e12c88f7820
batako <guri725ren213@gmail.com>
2018-02-25 17:34:57 +0900

    Refactor provisioner

--- server_script/bootstrap.sh
+++ server_script/bootstrap.sh
@@ -3,6 +3,8 @@
 __DIR__=$(cd $(dirname $0); pwd)
 SCRIPTS_PATH=$__DIR__/../vagrant_scripts
 APP_PATH=$__DIR__/../blockly
+SOCKETS_PATH=/var/run
+MINETEST_PATH=$__DIR__/../minetest
 
 echo Running bootstrap...
-APP_PATH=$APP_PATH SOCKETS_PATH=/var/run SCRIPTS_PATH=$SCRIPTS_PATH $SCRIPTS_PATH/vagrant.sh
+APP_PATH=$APP_PATH SOCKETS_PATH=$SOCKETS_PATH SCRIPTS_PATH=$SCRIPTS_PATH MINETEST_PATH=$MINETEST_PATH $SCRIPTS_PATH/vagrant.sh
--- vagrant_scripts/minetest/install_minetest.sh
+++ vagrant_scripts/minetest/install_minetest.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 __DIR__=$(cd $(dirname $0); pwd)
-ROOT=$__DIR__/../..
-MINETEST_PATH=$(cd $ROOT/minetest; pwd)
+MINETEST_PATH=${MINETEST_PATH:-$HOME/.minetest}
+MINETEST_USER=${MINETEST_USER:-$USER}
 
 echo Adding repository for minetest...
 curl -O http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
@@ -12,13 +12,10 @@ rm epel-release-latest-7.noarch.rpm
 echo Installing minetest...
 sudo yum install -y minetest
 
-echo Creating symbolic link to /var/lib/minetest/.minetest
-sudo -u minetest ln -s $MINETEST_PATH /var/lib/minetest/.minetest
-
-echo Changing owner of .minetest...
-sudo chown -R minetest.minetest $MINETEST_PATH
-
 echo Adding minetest-server service...
+cp $__DIR__/minetest-server.service.template $__DIR__/minetest-server.service
+grep -l 'MINETEST_USER' $__DIR__/minetest-server.service | xargs sed -i -e 's/MINETEST_USER/'$MINETEST_USER'/g'
+grep -l 'MINETEST_PATH' $__DIR__/minetest-server.service | xargs sed -i -e 's/MINETEST_PATH/'${MINETEST_PATH////\\/}'/g'
 sudo cp $__DIR__/minetest-server.service /etc/systemd/system/.
 sudo systemctl enable minetest-server
 sudo systemctl restart minetest-server

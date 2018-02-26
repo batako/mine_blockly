96055017f0383ecc74c258cb3bb9d4d761de301f
batako <guri725ren213@gmail.com>
2018-02-27 02:20:23 +0900

    Fix auto-start service errors

--- vagrant_scripts/blockly/run.sh
+++ vagrant_scripts/blockly/run.sh
@@ -2,6 +2,7 @@
 
 __DIR__=$(cd $(dirname $0); pwd)
 BIN_PATH=${BIN_PATH:-$HOME/.rbenv/shims}
+SOCKETS_PATH=${SOCKETS_PATH:-/var/run/blockly}
 
 cd $__DIR__/../../blockly
 
@@ -14,9 +15,16 @@ $BIN_PATH/rake db:migrate
 echo Precompiling assets...
 $BIN_PATH/rake assets:clean assets:precompile
 
+echo Creating $SOCKETS_PATH...
+/usr/bin/sudo /usr/bin/mkdir $SOCKETS_PATH
+
+echo Creating socket directory...
+/usr/bin/sudo mkdir -p $SOCKETS_PATH
+/usr/bin/sudo chown $USER.$USER $SOCKETS_PATH
+
 echo Running puma...
 RAILS_ENV=${RAILS_ENV:-production} \
-  SOCKETS_PATH=${SOCKETS_PATH:-/var/run/blockly} \
+  SOCKETS_PATH=$SOCKETS_PATH \
   SECRET_KEY_BASE=$($BIN_PATH/rake secret) \
   BLOCKLY_MOD_HOME=${BLOCKLY_MOD_HOME:-$(cd $__DIR__/../../minetest; pwd)/blockly} \
   $BIN_PATH/pumactl start

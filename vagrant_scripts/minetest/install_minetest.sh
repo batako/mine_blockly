13f2cf0f363f57b346a2278dc89bb6ca8f847a8c
batako <guri725ren213@gmail.com>
2018-02-26 23:41:39 +0900

    Fix mod loading error of minetest

--- vagrant_scripts/minetest/install_minetest.sh
+++ vagrant_scripts/minetest/install_minetest.sh
@@ -3,6 +3,9 @@
 __DIR__=$(cd $(dirname $0); pwd)
 MINETEST_PATH=${MINETEST_PATH:-$(cd $__DIR__/../../minetest; pwd)}
 MINETEST_USER=${MINETEST_USER:-$USER}
+MINETEST_HOME=/home/$MINETEST_USER/.minetest
+SAMPLE_WORLD_CONFIG=$__DIR__/../../minetest/worlds/world.mt.example
+WORLD_PATH=${WORLD_PATH:-$(cd $__DIR__/../../minetest/worlds/world; pwd)}
 
 echo Adding repository for minetest...
 curl -O http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
@@ -12,10 +15,35 @@ rm epel-release-latest-7.noarch.rpm
 echo Installing minetest...
 sudo yum install -y minetest
 
+echo Creating symbolic link to $MINETEST_HOME...
+if [ -L $MINETEST_HOME ]; then
+  sudo unlink $MINETEST_HOME
+elif [ -e $MINETEST_HOME ]; then
+  sudo mv $MINETEST_HOME $MINETEST_HOME.$(date +%Y%m%d%H%M%S%3N)
+fi
+sudo -u $MINETEST_USER ln -s $MINETEST_PATH $MINETEST_HOME
+
+echo Changing owner of $MINETEST_PATH...
+sudo chown -R $MINETEST_USER.$MINETEST_USER $MINETEST_PATH
+
+echo Copying world.mt.example to $WORLD_PATH/world.mt...
+if [ -e $WORLD_PATH/world.mt ]; then
+  echo "$WORLD_PATH/world.mt: File exists"
+  read -p "overwrite? (y/N): " INPUT
+  case $INPUT in
+    [yY]*)
+      sudo rm $WORLD_PATH/world.mt
+      sudo -u $MINETEST_USER mkdir -p $WORLD_PATH
+      sudo -u $MINETEST_USER cp $SAMPLE_WORLD_CONFIG $WORLD_PATH/world.mt
+    ;;
+  esac
+else
+  sudo -u $MINETEST_USER cp $SAMPLE_WORLD_CONFIG $WORLD_PATH/world.mt
+fi
+
 echo Adding minetest-server service...
 cp $__DIR__/minetest-server.service.template $__DIR__/minetest-server.service
 grep -l 'MINETEST_USER' $__DIR__/minetest-server.service | xargs sed -i -e 's/MINETEST_USER/'$MINETEST_USER'/g'
-grep -l 'MINETEST_PATH' $__DIR__/minetest-server.service | xargs sed -i -e 's/MINETEST_PATH/'${MINETEST_PATH////\\/}'/g'
 sudo cp $__DIR__/minetest-server.service /etc/systemd/system/.
 sudo systemctl enable minetest-server
 sudo systemctl restart minetest-server

dc21a1295714463ecb9c015cd358239105146fca
batako <guri725ren213@gmail.com>
2018-03-07 15:07:10 +0900

    Add dig blocker mod

---
+++ minetest/mods/dig_blocker/init.lua
@@ -0,0 +1,45 @@
+local old_node_dig = minetest.node_dig
+
+function minetest.node_dig(pos, node, digger)
+    if(pos.y <= 18)then
+       return
+    else
+       return old_node_dig(pos, node, digger)
+    end
+end
+
+local old_node_punch= minetest.node_punch
+
+function minetest.node_punch(pos, node, player, pointed_thing)
+     return
+end
+
+local old_item_use = minetest.item_use
+
+function minetest.item_use(itemstack, user, pointed_thing)
+     return
+end
+
+
+local old_item_place = minetest.item_place
+
+function minetest.item_place(itemstack2, placer, pointed_thing, param2)
+
+     if
+       (string.sub(itemstack2:get_name(),1,17) == "default:sign_wall") or
+       (string.sub(itemstack2:get_name(),1,8) == "flowers:") or
+       (string.sub(itemstack2:get_name(),1,13) == "default:torch") or
+       (string.sub(itemstack2:get_name(),1,23) == "default:mese_post_light")
+     then
+       return old_item_place(itemstack2, placer, pointed_thing, param2)
+     else
+       return
+     end
+end
+
+
+local old_item_drop = minetest.item_drop
+
+function minetest.item_drop(itemstack, dropper, pos)
+     return old_item_drop(itemstack, dropper, pos)
+end

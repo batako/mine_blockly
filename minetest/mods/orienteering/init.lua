fd70d7cbc04151fad99189f93b2c0a1aa48a86a0
batako <guri725ren213@gmail.com>
2018-03-07 15:03:03 +0900

    Add default values to orienteering mod

--- minetest/mods/orienteering/init.lua
+++ minetest/mods/orienteering/init.lua
@@ -256,7 +256,7 @@ end
 function orienteering.update_hud_displays(player)
 	local toDegrees=180/math.pi
 	local name = player:get_player_name()
-	local gps, altimeter, triangulator, compass, sextant, watch, speedometer, quadcorder
+	local gps, altimeter = true, triangulator = true, compass, sextant, watch, speedometer, quadcorder
 
 	if orienteering.tool_active(player, "orienteering:gps") then
 		gps = true

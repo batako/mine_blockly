b87b7d678b284846fcfa3e2605df581f1e18a91a
batako <guri725ren213@gmail.com>
2018-03-10 11:41:08 +0900

    Add bootstrap 4 (#26)

--- blockly/app/assets/javascripts/application.js
+++ blockly/app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require rails-ujs
-//= require jquery
 //= require rails-ujs
 //= require turbolinks
 //= require alertify

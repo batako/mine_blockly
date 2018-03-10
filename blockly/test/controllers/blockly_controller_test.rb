4376df8b3284e4d1d1d4103706838b7be56345a1
batako <guri725ren213@gmail.com>
2018-03-11 01:55:04 +0900

    Fix blockly test (#26)

--- blockly/test/controllers/blockly_controller_test.rb
+++ blockly/test/controllers/blockly_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class BlocklyControllerTest < ActionDispatch::IntegrationTest
+class BlockliesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get blockly_index_url
+    get blocklies_url
     assert_response :success
   end
 

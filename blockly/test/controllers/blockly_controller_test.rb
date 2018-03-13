a3695d604d7c9b428f601d2769ea87d53a2f1da2
batako <guri725ren213@gmail.com>
2018-03-13 12:53:54 +0900

    Remove minitest

--- blockly/test/controllers/blockly_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class BlockliesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get blocklies_url
-    assert_response :success
-  end
-
-end

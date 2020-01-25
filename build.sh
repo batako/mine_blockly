bf6f021fb5308e293777c2fe6cfa762cecca85c4
batako <guri725ren213@gmail.com>
2020-01-25 20:32:38 +0900

    Enable travis caching dependencies and directories

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-cd blockly
+cd $TRAVIS_BUILD_DIR/blockly
 gem install bundler
-bundle install --without development production
+bundle install -j$(getconf _NPROCESSORS_ONLN) --without development production --path vendor/bundle
 bin/rails db:migrate RAILS_ENV=test
 bundle exec rspec
